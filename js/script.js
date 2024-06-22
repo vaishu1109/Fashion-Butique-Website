ffc4715e6c82718ee9e333b1f5379e9c3c3835bd
vaishu1109 <vaishu.sg1109@gmail.com>
2024-06-22 21:02:48 +0530

    Created a slider

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// card js
+
+document.addEventListener("DOMContentLoaded", function () {
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach((card) => {
+      card.addEventListener("click", function () {
+        alert(`You clicked on ${card.querySelector("h3").innerText}`);
+      });
+    });
+  });
+
+
+//automatically change slider js
+
+const images = ["./img/logo.png","./img/blouse1.jpg","./img/kids1.jpg"];
+let currentIndex = 0;
+const slider = document.getElementById("slider");
+
+function changeImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    slider.src = images[currentIndex];
+}
+
+setInterval(changeImage, 3000);
